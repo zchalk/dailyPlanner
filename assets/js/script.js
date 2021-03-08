790054130a92255f38fc6f70a1842211afb0bb32
zchalk <you@example.com>
2021-03-08 14:06:22 -0500

    working java, still needs local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,32 +1,41 @@
-var eventContainer = document.getElementById("container");
-var currentDay = document.getElementById("currentDay");
+var eventContainer = $("#eventContainer");
+var currentDay =$("#currentDay");
 var today = moment ();
 var hoursInDay = ["9 AM","10 AM", "11 AM", "12 PM", "1 PM", "2 AM", "3 AM", "4 AM", "5 AM"];
+var eventObjects = [];
 
 
-var eventObjects = [{ 
+for (let i = 0; i < hoursInDay.length; i++) {
+  
+eventObjects.push( { 
     time: hoursInDay[i], // should i make a for loop for this instead? maybe this is the wrong order.. or i could create them all
-    day: today,
-    plan: textcontent.eventObjects[i].event,
-}]
+    // day: today,
+    plan: "",
+})}
 
 
 $(currentDay).text(today.format("MMM Do, YYYY"));
 
+console.log(eventContainer);
 
-eventObjects.forEach(function(eventObjects) {
-    $(eventContainer).add('div').addClass('timeContainers')
-    for (time in eventObjects) {
-        $('timeContainers').add('p').addClass('timeDisplay')
-    }
-    for (plan in eventObjects) { 
-        $('timeContainers').add('p').addClass('planDisplay')  
-    }
+eventObjects.forEach(function(eventObject) {
+    var div = $('<div>').addClass('timeContainers')
+    var timeDisplay = $('<p>').addClass('timeDisplay').text(eventObject.time)
+    var planDisplay = $('<input>').addClass('planDisplay').val(eventObject.plan)
+    var saveButton = $('<button>').text("save").data('hour',eventObject.time)
+    div.append(timeDisplay, planDisplay, saveButton)
+    eventContainer.append(div)
 })
 
-// add save button 
-
-saveButton.addEventListener("click", function(event) {
+$("button").on('click',function(event) {
+    var planText = $(this).siblings(".planDisplay").val()
+    var planHour = $(this).data('hour')
+    eventObjects.forEach(function(eventObject) {
+        if (planHour == eventObject.time)
+        eventObject.plan = planText
+    })
+    console
+    console.log(planText, planHour);
     JSON.stringify(eventObjects)
 })
 
@@ -35,8 +44,5 @@ saveButton.addEventListener("click", function(event) {
 // $(function() {
     // $("#currentDay").datepicker();
 //   });
-
-
-
 
 
