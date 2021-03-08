ea84139bfdff4ca60bf28ea6bdd2c243156bdea4
zoë <zoe@zoes-mbp.home>
2021-03-08 18:30:53 -0500

    java touches, it now saves

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 var eventContainer = $("#eventContainer");
-var currentDay =$("#currentDay");
+var currentDay = $("#currentDay");
 var today = moment ();
 var hoursInDay = ["9 AM","10 AM", "11 AM", "12 PM", "1 PM", "2 AM", "3 AM", "4 AM", "5 AM"];
 var eventObjects = [];
@@ -8,13 +8,12 @@ var eventObjects = [];
 for (let i = 0; i < hoursInDay.length; i++) {
   
 eventObjects.push( { 
-    time: hoursInDay[i], // should i make a for loop for this instead? maybe this is the wrong order.. or i could create them all
-    // day: today,
+    time: hoursInDay[i],
     plan: "",
 })}
 
 
-$(currentDay).text(today.format("MMM Do, YYYY"));
+$(currentDay).text(today.format("MMM Do, YYYY, h:mm"));
 
 console.log(eventContainer);
 
@@ -27,22 +26,28 @@ eventObjects.forEach(function(eventObject) {
     eventContainer.append(div)
 })
 
-$("button").on('click',function(event) {
+var refreshButton = $('<button>').text("refresh time").data('time',today);
+eventContainer.append(refreshButton)
+
+
+$("button").on('click', function(event) {
     var planText = $(this).siblings(".planDisplay").val()
     var planHour = $(this).data('hour')
     eventObjects.forEach(function(eventObject) {
         if (planHour == eventObject.time)
         eventObject.plan = planText
+        window.localStorage.setItem("eventObjects", JSON.stringify(eventObjects))
     })
-    console
-    console.log(planText, planHour);
-    JSON.stringify(eventObjects)
 })
 
-// add function to render on page
+$("document").on('click',refreshButton, function(event) {
+    today = moment();
+    $(currentDay).text(today.format("MMM Do, YYYY, h:mm"));
 
-// $(function() {
-    // $("#currentDay").datepicker();
-//   });
+})
 
-
+function pastEvents() {
+    for (let i = 0; i < iterable.length; i++) {
+      if ()
+    }
+}
