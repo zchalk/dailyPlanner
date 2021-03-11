09c3d69a90848f3dea100c60ade3132a246f2c62
zchalk <you@example.com>
2021-03-10 21:38:36 -0500

    data persists

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,7 @@ var currentDay = $("#currentDay");
 var today = moment ();
 var now = moment().hours();
 var hoursInDay = [ "9", "10", "11", "12", "13", "14", "15", "16", "17"];
-var eventObjects = [];
+var eventObjects = JSON.parse(localStorage.getItem("eventObjects")) || [];
 
 $(currentDay).text(today.format("MMM Do, YYYY, h:mm"));
 
@@ -18,36 +18,37 @@ if (now > 18) {
 }
 
 eventObjects.forEach(function(eventObject) {
-    var div = $('<div>').addClass('timeContainers').data('hour',eventObject.time)
+    var div = $('<div>').addClass('timeContainers').data('hour',eventObject.time);
     var timeDisplay = $('<p>').addClass('timeDisplay').text(eventObject.time);
     var planDisplay = $('<input>').addClass('planDisplay').val(eventObject.plan);
     var saveButton = $('<button>').addClass('saveButton').data('hour',eventObject.time);
 
-    div.append(timeDisplay, planDisplay, saveButton)
-    eventContainer.append(div)
+    div.append(timeDisplay, planDisplay, saveButton);
+    eventContainer.append(div);
+});
 
-})
+
 
 var refreshButton = $('<button>').text("refresh time").addClass('refreshButton').data('time',today);
-eventContainer.prepend(refreshButton)
+eventContainer.prepend(refreshButton);
 
 
 $("button.saveButton").on('click', function(event) {
-    var planText = $(this).siblings(".planDisplay").val()
-    var planHour = $(this).data('hour')
+    var planText = $(this).siblings(".planDisplay").val();
+    var planHour = $(this).data('hour');
     eventObjects.forEach(function(eventObject) {
         if (planHour == eventObject.time) {
-        eventObject.plan = planText
+        eventObject.plan = planText;
         window.localStorage.setItem("eventObjects", JSON.stringify(eventObjects))
-        }
-    })
-})
+        };
+    });
+});
 
 refreshButton.on('click', function(event) {
     today = moment();
     $(currentDay).text(today.format("MMM Do, YYYY, HH:mm"));
 
-})
+});
 
 $("div.timeContainers").each(function() {
     var planHour = $(this)
@@ -59,5 +60,5 @@ $("div.timeContainers").each(function() {
       }
       else {
           planHour.addClass('future');
-      }
-})
+      };
+});
